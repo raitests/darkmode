387e55abed88707009710edc97cea892774d4fd4
Pratik <rpratik6600@gmail.com>
2023-08-26 17:53:16 +0545

    paragraph update

--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -4,10 +4,12 @@ import classes from "./Paragraph.module.css";
 const Paragraph = () => {
   return (
     <p className={classes.paragraph}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Sit amet volutpat
-      consequat mauris nunc congue. Semper risus in hendrerit gravida rutrum
-      quisque non. Tortor consequat id porta nibh venenatis.
+      I stored theme in localStorage and used useState to manage the theme
+      state, used that state to toggle the css from light to dark or vice-versa
+      in the App.css. Using the same state, i handled another checked state for
+      the checkbox icon you see at the top, it uses the same theme state to
+      track the checked or unchecked state and toggles back and forth between
+      sun and moon.
     </p>
   );
 };
