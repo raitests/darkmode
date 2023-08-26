e60d7eb2f544ccfc41af45d6435c70351ceb4e22
Pratik <rpratik6600@gmail.com>
2023-08-26 19:58:17 +0545

    final changes

--- src/components/DemoOutput.jsx
+++ src/components/DemoOutput.jsx
@@ -6,8 +6,12 @@ const demoOutput = (props) => {
     <div>
       <h2>Learning to make darkmode</h2>
       <Paragraph />
-      <h3>Lean more</h3>
-      <Paragraph />
+      <h3>Referenced</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        enim modi porro nihil cumque magnam obcaecati! Eaque dolores in ratione
+        odit odio quaerat libero voluptate sint earum? Dolorem, est dolor.
+      </p>
     </div>
   );
 };
